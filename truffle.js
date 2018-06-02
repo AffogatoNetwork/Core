a664fd833681922c46afe94f8a3ecca641448b8a
Cristian Garner <ceegarner@gmail.com>
2018-06-02 11:00:10 -0600

    added migrations and compile

--- truffle.js
+++ truffle.js
@@ -1,9 +1,11 @@
 module.exports = {
-    networks: {
-         development: {
-              host: "localhost",
-              port: 8545,
-              network_id: "*" // Match any network id
-            }
-       }
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    }
+  }
 };
