1dfd7e67da187506a3cf5f4d90be41e684d0c484
Cristian Garner <ceegarner@gmail.com>
2018-06-06 08:54:44 -0600

    added export information to rinkeby

--- truffle.js
+++ truffle.js
@@ -1,3 +1,7 @@
+require('dotenv').config();
+var HDWalletProvider = require("truffle-hdwallet-provider-privkey");
+var pkey =  process.env.TESTING_PKEY;
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -6,6 +10,18 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
+    },
+    /*rinkeby: {
+      host: "127.0.0.1", // Connect to geth on the specified
+      port: 8545,
+      from: "0x5786e4d14e9027395146c4301c8af60963b66819", // default address to use for any transaction Truffle makes during migrations
+      network_id: 4,
+      gas: 4612388 // Gas limit used for deploys
+    },*/
+    "rinkeby-infura": {
+      provider: () => new HDWalletProvider(pkey, "https://rinkeby.infura.io/"+ process.env.INFURA_KEY),
+      network_id: 4,
+      gas: 4700000
     }
   }
 };
