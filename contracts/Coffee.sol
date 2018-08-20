6da07f2cd4118709e665f90e87d65d32bc1b91d8
Cristian Garner <ceegarner@gmail.com>
2018-08-20 15:50:52 -0600

    refactored Coffee Batch Contract

--- contracts/Coffee.sol
+++ contracts/Coffee.sol
@@ -2,197 +2,67 @@ pragma solidity ^0.4.23;
 
 contract Coffee{
 
-    event LogAddCoffeeBatch(uint256 indexed _id);
-    event LogAddCoffeeBatchAction(uint256 indexed _id);
-    event LogAddCoffeeBatchTasting(uint256 indexed _id);
-    event LogAddCoffeeBatchCertficate(uint256 indexed _id);
+    event LogAddCoffeeBatch(bytes32 indexed _id);
      
     struct CoffeeBatch{
-        uint256 id;
+        bytes32 uid;
+        bytes32 farmUid;
         uint16 altitude;
         bytes32 variety;
-        string lastCuppingNote; 
-        bool isSold;
-        mapping(bytes32 => Action) actions;
-        bytes32[] actionIds;
-        mapping(address => CupProfile) cupProfiles;
-        address[] tasters;
-        Certificate[] certificates;
-        string additionalInformation;   
-    }
-    
-    struct Action{
-        //@dev address of the individual or the organization who realizes the action.
-        address processor;
-        //@dev description of the action.
-        bytes32 typeOfAction; 
-        string additionalInformation;
-        // @dev Instant of time when the Action is done.
-        uint timestamp;
+        bytes32 process;
+        //QQ - Precision two decimals 100.00
+        uint32 size;
+        bool isSold;  
     }
 
-    struct CupProfile{
-        bytes32 aroma;
-        bytes32 flavor;
-        bytes32 acidity;
-        bytes32 body;
-        bytes32 aftertaste;
-        string cuppingNote;
-        string additionalInformation;
-        uint timestamp;
-    }
-
-    struct Certificate{
-        address certifier;
-        bytes32 typeOfCertificate; 
-        string additionalInformation;
-        // @dev Instant of time when the Action is done.
-        uint timestamp;
-    }
-
-    CoffeeBatch[] public coffeeBatches;
+   mapping(bytes32 => CoffeeBatch) public coffeeBatches;
+   mapping(bytes32 => bytes32[]) public farmToBatches;
    // mapping(uint256 => CoffeeBatch) coffeeBatches;
    // uint256[] coffeeBatchIds;
+   uint coffeeBatchCount = 0;
 
-    function getCount() public view returns(uint count) {
-        return coffeeBatches.length;
+    function getFarmCoffeeBatchCount(bytes32 _farmUid) public view returns(uint count) {
+        return farmToBatches[_farmUid].length;
     }
 
-    function getCoffeeBatchTastersCount(uint _id) public view returns(uint count) {
-        return coffeeBatches[_id].tasters.length;
-    }
-
-    function getCoffeeBatchCertificatesCount(uint _id) public view returns(uint count) {
-        return coffeeBatches[_id].certificates.length;
-    }
-
-    function getCoffeeBatchActions(uint _coffeeBatchIndex) public view returns(bytes32[]){
-        CoffeeBatch memory coffeeBatch = coffeeBatches[_coffeeBatchIndex];
-        return coffeeBatch.actionIds;
-    }
-
-    function getCoffeeBatchAction(uint _coffeeBatchIndex, bytes32 _typeOfaction) 
-    public view returns (address,bytes32,string,uint){
-        CoffeeBatch storage coffeeBatch = coffeeBatches[_coffeeBatchIndex];
-        Action memory action = coffeeBatch.actions[_typeOfaction];
-        return (
-            action.processor,
-            action.typeOfAction,
-            action.additionalInformation,
-            action.timestamp
-        );
-    }
-
-    function getCoffeeBatchTasters(uint _coffeeBatchIndex) public view returns(address[]){
-        CoffeeBatch memory coffeeBatch = coffeeBatches[_coffeeBatchIndex];
-        return coffeeBatch.tasters;
-    }
-
-    function getCoffeeBatchCertificate(uint _coffeeBatchIndex, uint _certificateId) 
-    public view returns(
-        address,
+    function getCoffeeBatchById(bytes32 _uid) public view returns(
         bytes32,
-        string,
-        uint
+        bytes32,
+        uint16,
+        bytes32,
+        bytes32,
+        uint32,
+        bool
     ){
-        CoffeeBatch memory coffeeBatch = coffeeBatches[_coffeeBatchIndex];
-        Certificate memory certificate = coffeeBatch.certificates[_certificateId];
+        CoffeeBatch memory coffeeBatch = coffeeBatches[_uid];
         return(
-            certificate.certifier,
-            certificate.typeOfCertificate,
-            certificate.additionalInformation,
-            certificate.timestamp
-        );
-    }
-
-    function getCoffeeBatchCupProfile(uint _coffeeBatchIndex, address _taster) public view returns(
-        bytes32,
-        bytes32,
-        bytes32,
-        bytes32,
-        bytes32,
-        string,
-        string,
-        uint
-    ){
-        CoffeeBatch storage coffeeBatch = coffeeBatches[_coffeeBatchIndex];
-        CupProfile memory cupProfile = coffeeBatch.cupProfiles[_taster];
-        return(
-            cupProfile.aroma,
-            cupProfile.flavor,
-            cupProfile.acidity,
-            cupProfile.body,
-            cupProfile.aftertaste,
-            cupProfile.cuppingNote,
-            cupProfile.additionalInformation,
-            cupProfile.timestamp
-        );
-    }
-
-    function addCoffeeBatch(uint16 _altitude, bytes32 _variety, string _additionalInformation, uint _timestamp) public {
-        Action memory action = Action(msg.sender,"creation",_additionalInformation, _timestamp); 
-        //Fixes memory error that doesn't allow to create memory objects in structs
-        coffeeBatches.length++;
-        CoffeeBatch storage coffeeBatch = coffeeBatches[coffeeBatches.length - 1];
-        //Assigns values
-        coffeeBatch.id = coffeeBatches.length - 1;
-        coffeeBatch.altitude = _altitude;
-        coffeeBatch.variety = _variety;
-        coffeeBatch.lastCuppingNote = "";
-        coffeeBatch.isSold = false;
-        coffeeBatch.actions["creation"] = action;
-        coffeeBatch.actionIds.push("creation");
-        coffeeBatch.additionalInformation = _additionalInformation;
-        //logs insert
-        emit LogAddCoffeeBatch(coffeeBatch.id);        
-    }
-
-    function addCoffeeBatchAction(uint _coffeeBatchId, bytes32 _typeOfAction, string _additionalInformation, uint _timestamp) public {
-        Action memory action = Action(msg.sender,_typeOfAction,_additionalInformation, _timestamp); 
-        CoffeeBatch storage coffeeBatch = coffeeBatches[_coffeeBatchId];
-        coffeeBatch.actions[_typeOfAction] = action;
-        coffeeBatch.actionIds.push(_typeOfAction);
-        emit LogAddCoffeeBatchAction(coffeeBatch.id);   
-    }
-
-    function addCoffeeBatchTasting(
-        uint _coffeeBatchId,
-        bytes32 _aroma, 
-        bytes32 _flavor, 
-        bytes32 _acidity, 
-        bytes32 _body, 
-        bytes32 _aftertaste, 
-        string _cuppingNote, 
-        string _additionalInformation,
-        uint _timestamp
-    ) public {
-        CupProfile memory cupProfile = CupProfile(
-            _aroma, 
-            _flavor, 
-            _acidity, 
-            _body, 
-            _aftertaste, 
-            _cuppingNote, 
-            _additionalInformation,
-            _timestamp
+            coffeeBatch.uid,
+            coffeeBatch.farmUid,
+            coffeeBatch.altitude,
+            coffeeBatch.variety,
+            coffeeBatch.process,
+            coffeeBatch.size,
+            coffeeBatch.isSold
         ); 
-        CoffeeBatch storage coffeeBatch = coffeeBatches[_coffeeBatchId];
-        coffeeBatch.lastCuppingNote = _cuppingNote;
-        coffeeBatch.cupProfiles[msg.sender] = cupProfile;
-        coffeeBatch.tasters.push(msg.sender); 
-        emit LogAddCoffeeBatchTasting(coffeeBatch.id); 
     }
 
-    function addCoffeeBatchCertificate(
-        uint _coffeeBatchId,
-        bytes32 _typeOfCertificate,
-        string _additionalInformation,
-        uint _timestamp
-    ) public {
-        Certificate memory certificate = Certificate(msg.sender, _typeOfCertificate, _additionalInformation, _timestamp);
-        CoffeeBatch storage coffeeBatch = coffeeBatches[_coffeeBatchId];
-        coffeeBatch.certificates.push(certificate);
-        emit LogAddCoffeeBatchCertficate(coffeeBatch.id); 
+    function addCoffeeBatch(bytes32 _farmUid, uint16 _altitude, bytes32 _variety, bytes32 _process, uint32 _size) public {
+     //   Action memory action = Action(msg.sender,"creation",_additionalInformation, _timestamp); 
+        //Fixes memory error that doesn't allow to create memory objects in structs
+        bytes32 uid = keccak256(toBytes(coffeeBatchCount));
+        CoffeeBatch memory coffeeBatch = CoffeeBatch(uid, _farmUid, _altitude, _variety, _process, _size, false);
+        coffeeBatchCount++;
+        coffeeBatches[uid] = coffeeBatch;
+        farmToBatches[_farmUid].push(uid);
+        emit LogAddCoffeeBatch(uid);        
+    }
+
+    //TODO: pass to a util function
+    function toBytes(uint256 x)  internal pure returns (bytes b) {
+        b = new bytes(32);
+        for (uint i = 0; i < 32; i++) {
+            b[i] = byte(uint8(x / (2**(8*(31 - i))))); 
+        }
     }
 
 
--- test/Coffee.js
+++ test/Coffee.js
@@ -1,129 +1,310 @@
 var Coffee = artifacts.require("./Coffee.sol");
 
-contract(Coffee,function(accounts){
-    var tokenInstance;
-    var timeNow;
-    
-    function byteToString(a){
-       return trimNull(web3.toUtf8(a));
+contract(Coffee, function(accounts) {
+  var tokenInstance;
+  var timeNow;
+
+  function byteToString(a) {
+    return trimNull(web3.toUtf8(a));
+  }
+
+  function trimNull(a) {
+    var c = a.indexOf("\0");
+    if (c > -1) {
+      return a.substr(0, c);
     }
+    return a;
+  }
 
-    function trimNull(a) {
-        var c = a.indexOf('\0');
-        if (c>-1) {
-          return a.substr(0, c);
-        }
-        return a;
-    }
-	
-	it('Adds a Coffee Batch',function(){
-		return Coffee.deployed().then(function(instance){
-            tokenInstance = instance;
-            timeNow = + new Date();
-			return tokenInstance.addCoffeeBatch(
-                1200, 
-                "Catuai Rojo",
-                '{"size":"1","symbol":"QQ}',
-                timeNow,
-                {from: accounts[0]}
-            );
-		}).then(function(receipt){
-			assert.equal(receipt.logs.length, 1, 'triggers one event');
-      		assert.equal(receipt.logs[0].event, 'LogAddCoffeeBatch', 'should be the "LogAddCoffeeBatch" event');
-      		assert.equal(receipt.logs[0].args._id.toNumber(), 0, 'logs the inserted address id');
-      		return tokenInstance.getCount();
-		}).then(function(count){
-            assert.equal(1,count,'coffeeBatchIds should had incremented');
-            return tokenInstance.coffeeBatches(0);
-        }).then(function(coffeeBatch){
-            assert.equal(coffeeBatch[0], 0, 'id is equal to inserted');
-            assert.equal(coffeeBatch[1].toNumber(),1200, 'altitude is equal to inserted');
-            assert.equal(byteToString(coffeeBatch[2]), "Catuai Rojo", 'variety is equal to inserted');
-            //Web3 Doesn't support array of structs so they skip it
-            assert.equal(coffeeBatch[5], '{"size":"1","symbol":"QQ}');
-        });
-    });
+  it("Adds a Coffee Batch", function() {
+    return Coffee.deployed()
+      .then(function(instance) {
+        tokenInstance = instance;
+        timeNow = +new Date();
+        return tokenInstance.addCoffeeBatch(
+          "0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563",
+          1200,
+          "Catuai Rojo",
+          "Washed",
+          10000,
+          { from: accounts[0] }
+        );
+      })
+      .then(function(receipt) {
+        assert.equal(receipt.logs.length, 1, "triggers one event");
+        assert.equal(
+          receipt.logs[0].event,
+          "LogAddCoffeeBatch",
+          'should be the "LogAddCoffeeBatch" event'
+        );
+        assert.equal(
+          receipt.logs[0].args._id,
+          "0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563",
+          "logs the inserted uid"
+        );
+        return tokenInstance.getFarmCoffeeBatchCount(
+          "0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563"
+        );
+      })
+      .then(function(count) {
+        assert.equal(count, 1, "coffeeBatchIds should had incremented");
+        return tokenInstance.getCoffeeBatchById(
+          "0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563"
+        );
+      })
+      .then(function(coffeeBatch) {
+        assert.equal(
+          coffeeBatch[0],
+          "0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563",
+          "batch id is equal to generated"
+        );
+        assert.equal(
+          coffeeBatch[1],
+          "0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563",
+          "farm uid is equal to inserted"
+        );
+        assert.equal(
+          coffeeBatch[2].toNumber(),
+          1200,
+          "altitude is equal to inserted"
+        );
+        assert.equal(
+          byteToString(coffeeBatch[3]),
+          "Catuai Rojo",
+          "variety is equal to inserted"
+        );
+        assert.equal(
+          byteToString(coffeeBatch[4]),
+          "Washed",
+          "process is equal to inserted"
+        );
+        assert.equal(coffeeBatch[5], 10000, "size is equal to inserted");
+      });
+  });
+  /*
+  it("Handles Coffee Batch Actions", function() {
+    return Coffee.deployed()
+      .then(function(instance) {
+        tokenInstance = instance;
+        return tokenInstance.getCoffeeBatchAction(0, "creation");
+      })
+      .then(function(action) {
+        assert.equal(action[0], accounts[0], "address is equal to inserted");
+        assert.equal(
+          byteToString(action[1]),
+          "creation",
+          "type of action is equal to inserted"
+        );
+        assert.equal(
+          action[2],
+          '{"size":"1","symbol":"QQ}',
+          "additional information is equal to inserted"
+        );
+        assert.equal(action[3], timeNow, "time is equal to inserted");
+        return tokenInstance.getCoffeeBatchActions(0);
+      })
+      .then(function(actions) {
+        assert.equal(
+          byteToString(actions[0]),
+          "creation",
+          "Current only action is creation"
+        );
+        timeNow = +new Date();
+        return tokenInstance.addCoffeeBatchAction(
+          0,
+          "depulped",
+          '{"size":"0.5","symbol":"QQ}',
+          timeNow,
+          { from: accounts[0] }
+        );
+      })
+      .then(function(receipt) {
+        assert.equal(receipt.logs.length, 1, "triggers one event");
+        assert.equal(
+          receipt.logs[0].event,
+          "LogAddCoffeeBatchAction",
+          'should be the "LogAddCoffeeBatch" event'
+        );
+        assert.equal(
+          receipt.logs[0].args._id.toNumber(),
+          0,
+          "logs the inserted address id"
+        );
+        return tokenInstance.getCoffeeBatchActions(0);
+      })
+      .then(function(actions) {
+        assert.equal(
+          byteToString(actions[0]),
+          "creation",
+          "Action is creation"
+        );
+        assert.equal(
+          byteToString(actions[1]),
+          "depulped",
+          "Action is depulped"
+        );
+        return tokenInstance.getCoffeeBatchAction(0, "depulped");
+      })
+      .then(function(action) {
+        assert.equal(action[0], accounts[0], "address is equal to inserted");
+        assert.equal(
+          byteToString(action[1]),
+          "depulped",
+          "type of action is equal to inserted"
+        );
+        assert.equal(
+          action[2],
+          '{"size":"0.5","symbol":"QQ}',
+          "additional information is equal to inserted"
+        );
+        assert.equal(action[3], timeNow, "time is equal to inserted");
+      });
+  });
 
-    it('Handles Coffee Batch Actions',function(){
-        return Coffee.deployed().then(function(instance){
-            tokenInstance = instance;
-            return tokenInstance.getCoffeeBatchAction(0,"creation");
-        }).then(function(action){
-            assert.equal(action[0], accounts[0], 'address is equal to inserted');
-            assert.equal(byteToString(action[1]), "creation", 'type of action is equal to inserted');
-            assert.equal(action[2], '{"size":"1","symbol":"QQ}', 'additional information is equal to inserted');
-            assert.equal(action[3], timeNow, 'time is equal to inserted');
-            return tokenInstance.getCoffeeBatchActions(0);
-        }).then(function(actions){
-            assert.equal(byteToString(actions[0]), "creation", 'Current only action is creation');
-            timeNow = + new Date();
-            return tokenInstance.addCoffeeBatchAction(0,"depulped",'{"size":"0.5","symbol":"QQ}',timeNow,{from: accounts[0]});
-        }).then(function(receipt){
-			assert.equal(receipt.logs.length, 1, 'triggers one event');
-      		assert.equal(receipt.logs[0].event, 'LogAddCoffeeBatchAction', 'should be the "LogAddCoffeeBatch" event');
-            assert.equal(receipt.logs[0].args._id.toNumber(), 0, 'logs the inserted address id');
-            return tokenInstance.getCoffeeBatchActions(0);
-        }).then(function(actions){
-            assert.equal(byteToString(actions[0]), "creation", 'Action is creation');
-            assert.equal(byteToString(actions[1]), "depulped", 'Action is depulped');
-            return tokenInstance.getCoffeeBatchAction(0,"depulped");
-        }).then(function(action){
-            assert.equal(action[0], accounts[0], 'address is equal to inserted');
-            assert.equal(byteToString(action[1]), "depulped", 'type of action is equal to inserted');
-            assert.equal(action[2], '{"size":"0.5","symbol":"QQ}', 'additional information is equal to inserted');
-            assert.equal(action[3], timeNow, 'time is equal to inserted');
-        }); 
-    });
+  it("Handles Coffee Batch Tasting", function() {
+    return Coffee.deployed()
+      .then(function(instance) {
+        tokenInstance = instance;
+        timeNow = +new Date();
+        return tokenInstance.addCoffeeBatchTasting(
+          0,
+          "Floral Dulce",
+          "Naranja",
+          "Cítrica suave",
+          "Cremoso",
+          "Floral y entonante",
+          "90.0",
+          '{"notes":"Bebida agradable al paladar"}',
+          timeNow,
+          { from: accounts[1] }
+        );
+      })
+      .then(function(receipt) {
+        assert.equal(receipt.logs.length, 1, "triggers one event");
+        assert.equal(
+          receipt.logs[0].event,
+          "LogAddCoffeeBatchTasting",
+          'should be the "LogAddCoffeeBatchTasting" event'
+        );
+        assert.equal(
+          receipt.logs[0].args._id.toNumber(),
+          0,
+          "logs the inserted address id"
+        );
+        return tokenInstance.getCoffeeBatchTasters(0);
+      })
+      .then(function(tasters) {
+        assert.equal(tasters[0], accounts[1], "taster needs to be the same");
+        return tokenInstance.getCoffeeBatchCupProfile(0, tasters[0]);
+      })
+      .then(function(cupProfile) {
+        assert.equal(
+          byteToString(cupProfile[0]),
+          "Floral Dulce",
+          "aroma is equal to inserted"
+        );
+        assert.equal(
+          byteToString(cupProfile[1]),
+          "Naranja",
+          "flavor is equal to inserted"
+        );
+        assert.equal(
+          byteToString(cupProfile[2]),
+          "Cítrica suave",
+          "acidity is equal to inserted"
+        );
+        assert.equal(
+          byteToString(cupProfile[3]),
+          "Cremoso",
+          "body is equal to inserted"
+        );
+        assert.equal(
+          byteToString(cupProfile[4]),
+          "Floral y entonante",
+          "after taste is equal to inserted"
+        );
+        assert.equal(
+          cupProfile[5],
+          "90.0",
+          "cupping note is equal to inserted"
+        );
+        assert.equal(
+          cupProfile[6],
+          '{"notes":"Bebida agradable al paladar"}',
+          "additional Data is equal to inserted"
+        );
+        assert.equal(cupProfile[7], timeNow, "time is equal to inserted");
+        timeNow = +new Date();
+        return tokenInstance.addCoffeeBatchTasting(
+          0,
+          "Floral Dulce",
+          "Naranja y mandarina",
+          "Cítrica suave",
+          "Cremoso",
+          "Floral y persistente",
+          "92.0",
+          '{"notes":"Muy buena"}',
+          timeNow,
+          { from: accounts[2] }
+        );
+      })
+      .then(function(receipt) {
+        return tokenInstance.getCoffeeBatchTastersCount(0);
+      })
+      .then(function(count) {
+        assert.equal(count, 2, "Tasters size increase");
+      });
+  });
 
-    it('Handles Coffee Batch Tasting',function(){
-        return Coffee.deployed().then(function(instance){
-            tokenInstance = instance;
-            timeNow = + new Date();
-            return tokenInstance.addCoffeeBatchTasting(0,'Floral Dulce','Naranja','Cítrica suave','Cremoso','Floral y entonante','90.0','{"notes":"Bebida agradable al paladar"}',timeNow,{from:accounts[1]});
-        }).then(function(receipt){
-			assert.equal(receipt.logs.length, 1, 'triggers one event');
-      		assert.equal(receipt.logs[0].event, 'LogAddCoffeeBatchTasting', 'should be the "LogAddCoffeeBatchTasting" event');
-            assert.equal(receipt.logs[0].args._id.toNumber(), 0, 'logs the inserted address id');
-            return tokenInstance.getCoffeeBatchTasters(0);
-        }).then(function(tasters){
-            assert.equal(tasters[0], accounts[1],'taster needs to be the same');
-            return tokenInstance.getCoffeeBatchCupProfile(0,tasters[0]);
-        }).then(function(cupProfile){
-            assert.equal(byteToString(cupProfile[0]),'Floral Dulce','aroma is equal to inserted');
-            assert.equal(byteToString(cupProfile[1]),'Naranja','flavor is equal to inserted');
-            assert.equal(byteToString(cupProfile[2]),'Cítrica suave','acidity is equal to inserted');
-            assert.equal(byteToString(cupProfile[3]),'Cremoso','body is equal to inserted');
-            assert.equal(byteToString(cupProfile[4]),'Floral y entonante','after taste is equal to inserted');
-            assert.equal(cupProfile[5],'90.0','cupping note is equal to inserted');
-            assert.equal(cupProfile[6],'{"notes":"Bebida agradable al paladar"}','additional Data is equal to inserted');
-            assert.equal(cupProfile[7], timeNow, 'time is equal to inserted');
-            timeNow = + new Date();
-            return tokenInstance.addCoffeeBatchTasting(0,'Floral Dulce','Naranja y mandarina','Cítrica suave','Cremoso','Floral y persistente','92.0','{"notes":"Muy buena"}',timeNow,{from:accounts[2]});
-        }).then(function(receipt){
-            return tokenInstance.getCoffeeBatchTastersCount(0);
-        }).then(function(count){
-            assert.equal(count,2, 'Tasters size increase');
-        }); 
-    });
-
-    it('Handles Coffee Batch Certificates', function(){
-        return Coffee.deployed().then(function(instance){
-            tokenInstance = instance;
-            timeNow = + new Date();
-            return tokenInstance.addCoffeeBatchCertificate(0,'Denominación de origen Marcala','{"notes":"Paso todas las pruebas de catación, análisis de suelo."}',timeNow,{from:accounts[1]});
-        }).then(function(receipt){
-            assert.equal(receipt.logs.length, 1, 'triggers one event');
-      		assert.equal(receipt.logs[0].event, 'LogAddCoffeeBatchCertficate', 'should be the "LogAddCoffeeBatchCertficate" event');
-            assert.equal(receipt.logs[0].args._id.toNumber(), 0, 'logs the inserted address id');
-            return tokenInstance.getCoffeeBatchCertificatesCount(0);
-        }).then(function(count){
-            assert.equal(count,1,'certificates number equal to 1');
-            return tokenInstance.getCoffeeBatchCertificate(0,0);
-        }).then(function(certificate){
-            assert.equal(certificate[0],accounts[1],'address is equal to inserted');
-            assert.equal(byteToString(certificate[1]), "Denominación de origen Marcala", 'type of certificate is equal to inserted');
-            assert.equal(certificate[2], '{"notes":"Paso todas las pruebas de catación, análisis de suelo."}', 'additional information is equal to inserted');
-            assert.equal(certificate[3], timeNow, 'time is equal to inserted');
-        });
-    });
+  it("Handles Coffee Batch Certificates", function() {
+    return Coffee.deployed()
+      .then(function(instance) {
+        tokenInstance = instance;
+        timeNow = +new Date();
+        return tokenInstance.addCoffeeBatchCertificate(
+          0,
+          "Denominación de origen Marcala",
+          '{"notes":"Paso todas las pruebas de catación, análisis de suelo."}',
+          timeNow,
+          { from: accounts[1] }
+        );
+      })
+      .then(function(receipt) {
+        assert.equal(receipt.logs.length, 1, "triggers one event");
+        assert.equal(
+          receipt.logs[0].event,
+          "LogAddCoffeeBatchCertficate",
+          'should be the "LogAddCoffeeBatchCertficate" event'
+        );
+        assert.equal(
+          receipt.logs[0].args._id.toNumber(),
+          0,
+          "logs the inserted address id"
+        );
+        return tokenInstance.getCoffeeBatchCertificatesCount(0);
+      })
+      .then(function(count) {
+        assert.equal(count, 1, "certificates number equal to 1");
+        return tokenInstance.getCoffeeBatchCertificate(0, 0);
+      })
+      .then(function(certificate) {
+        assert.equal(
+          certificate[0],
+          accounts[1],
+          "address is equal to inserted"
+        );
+        assert.equal(
+          byteToString(certificate[1]),
+          "Denominación de origen Marcala",
+          "type of certificate is equal to inserted"
+        );
+        assert.equal(
+          certificate[2],
+          '{"notes":"Paso todas las pruebas de catación, análisis de suelo."}',
+          "additional information is equal to inserted"
+        );
+        assert.equal(certificate[3], timeNow, "time is equal to inserted");
+      });
+  });
+  */
 });
