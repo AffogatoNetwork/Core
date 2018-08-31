3f040b036b190278235a22e53e355af7d84a9d48
Cristian Garner <ceegarner@gmail.com>
2018-08-30 21:00:40 -0600

    adds cup tasting

--- contracts/TastingFactory.sol
+++ contracts/TastingFactory.sol
@@ -5,6 +5,7 @@ import "./Utils.sol";
 contract TastingFactory is Utils{
 
    event LogAddCupProfile(bytes32 indexed _id);
+   event LogUpdateCupProfile(bytes32 indexed _id);
 
    struct CupProfile{
         bytes32 uid;
@@ -30,6 +31,26 @@ contract TastingFactory is Utils{
         return coffeeBatchToCupProfiles[_coffeeBatch].length;
     }
 
+    function getCupProfileById(bytes32 uid) public view returns(
+        bytes32,
+        bytes32, 
+        bytes32, 
+        bytes32, 
+        bytes32,
+        bytes32, 
+        uint16
+        ){
+        CupProfile memory cupProfile = cupProfiles[uid];
+        return(
+            cupProfile.uid,
+            cupProfile.aroma,
+            cupProfile.flavor,
+            cupProfile.acidity,
+            cupProfile.body,
+            cupProfile.aftertaste,
+            cupProfile.cuppingNote
+        );
+    }
 
     function addCupProfile(
         bytes32 _coffeeBatchId,
@@ -47,5 +68,25 @@ contract TastingFactory is Utils{
         cupProfiles[uid] = cupProfile;
         tastingCount++;
         emit LogAddCupProfile(uid);
+    }
+    //Coffee Batch can't be updated
+    function updateCupProfileById(
+        bytes32 _uid,
+        bytes32 _aroma,
+        bytes32 _flavor,
+        bytes32 _acidity,
+        bytes32 _body,
+        bytes32 _aftertaste,
+        uint16 _cuppingNote
+    ) public {
+        require(cupProfiles[_uid].aroma != 0);
+        CupProfile storage cupProfile = cupProfiles[_uid];
+        cupProfile.aroma = _aroma;
+        cupProfile.flavor = _flavor;
+        cupProfile.acidity = _acidity;
+        cupProfile.body = _body;
+        cupProfile.aftertaste = _aftertaste;
+        cupProfile.cuppingNote = _cuppingNote;
+        emit LogUpdateCupProfile(_uid);
     }
 }
--- test/TastingFactory.js
+++ test/TastingFactory.js
@@ -21,7 +21,7 @@ contract(TastingFactory, function(accounts) {
   });
 
   describe("Tasting Validations", () => {
-    it("Adds a cupProfile", async () => {
+    it("Adds a cup profile", async () => {
       const receipt = await this.tokenInstance.addCupProfile(
         "0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563",
         "Caramelo",
@@ -58,8 +58,52 @@ contract(TastingFactory, function(accounts) {
       );
     });
 
-    //   it("Gets a tasting", async () => {});
+    it("Gets a cup profile", async () => {
+      const cupProfile = await this.tokenInstance.getCupProfileById(
+        "0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563"
+      );
+      cupProfile[0].should.be.equal(
+        "0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563"
+      );
+      expect(byteToString(cupProfile[1])).to.be.equal("Caramelo");
+      expect(byteToString(cupProfile[2])).to.be.equal("Citrico");
+      expect(byteToString(cupProfile[3])).to.be.equal("balanceada");
+      expect(byteToString(cupProfile[4])).to.be.equal("balanceado");
+      expect(byteToString(cupProfile[5])).to.be.equal("seco");
+      expect(cupProfile[6].toNumber()).to.be.equal(8000);
+    });
 
-    //   it("Updates a tasting", async () => {});
+    it("Updates a cup profile", async () => {
+      const receipt = await this.tokenInstance.updateCupProfileById(
+        "0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563",
+        "Rosas",
+        "Citrico 2",
+        "balanceada 2",
+        "balanceado 2",
+        "Prolongado 2",
+        9000
+      );
+
+      receipt.logs.length.should.be.equal(1, "trigger one event");
+      receipt.logs[0].event.should.be.equal(
+        "LogUpdateCupProfile",
+        "should be the LogUpdateCupProfile event"
+      );
+      receipt.logs[0].args._id.should.be.equal(
+        "0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563",
+        "logs the yodated cup profile id"
+      );
+
+      const cupProfile = await this.tokenInstance.getCupProfileById(
+        "0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563"
+      );
+
+      expect(byteToString(cupProfile[1])).to.be.equal("Rosas");
+      expect(byteToString(cupProfile[2])).to.be.equal("Citrico 2");
+      expect(byteToString(cupProfile[3])).to.be.equal("balanceada 2");
+      expect(byteToString(cupProfile[4])).to.be.equal("balanceado 2");
+      expect(byteToString(cupProfile[5])).to.be.equal("Prolongado 2");
+      expect(cupProfile[6].toNumber()).to.be.equal(9000);
+    });
   });
 });
