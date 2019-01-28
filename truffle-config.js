abc1487bba762cce7aa295a8d0675ce1190a2602
Cristian Espinoza Garner <ceegarner@gmail.com>
2019-01-27 20:16:58 -0600

    deploy to rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,23 @@
+require("dotenv").config(); // Store environment-specific variable from '.env' to process.env
+const path = require("path");
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var mnemonic = process.env.MNENOMIC;
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      // must be a thunk, otherwise truffle commands may hang in CI
+      provider: () =>
+        new HDWalletProvider(mnemonic, process.env.RINKEBY_API_URL),
+      network_id: "4"
+    }
+  }
 };
