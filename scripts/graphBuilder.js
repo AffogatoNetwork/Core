26b8305cfa9fc083a68096c40ea9327e33fe5ee6
Isaac M <imtz90b@gmail.com>
2019-09-16 18:07:40 -0600

    WIP> replacing the contract address for the graph definition

--- scripts/graphBuilder.js
+++ scripts/graphBuilder.js
@@ -1,15 +1,28 @@
 
 const fs = require('fs');
 const path = require("path");
+var shell = require('shelljs');
 
 const argv = require('minimist')(process.argv.slice(2));
+
+fs.readFileAsync = function(filename, enc) {
+  return new Promise(function(resolve, reject) {
+      fs.readFile(filename, enc, function(err, data){
+          if (err) 
+              reject(err); 
+          else
+              resolve(data);
+      });
+  });
+};
+
 
 const network = argv["network"];
 
 if (!network){
   throw new Error("Network argument is required");
 }
-console.log(argv)
+
 var Contracts = [
   "ActorFactory",
   "CertificateFactory",
@@ -18,10 +31,15 @@ var Contracts = [
   "FarmFactory"
 ];
 
-var mappings = []
-mappings = Contracts.map((contract) =>{
-  const contents =  fs.readFileSync(path.resolve(__dirname, `../build/contracts/${contract}.json`), 'utf8');
-  contractObject =  JSON.parse(contents);
-  console.log(contractObject.networks[network].address)
-  return {contractName: contractObject.contractName, abi: contractObject.abi};
+shell.cp('-R', path.resolve(__dirname, `../sg_affogato/subgraph_template.yaml`), path.resolve(__dirname, `../sg_affogato/subgraph.yaml`))
+const mappings = Contracts.map((contract) => {
+  const fileData = fs.readFileSync(path.resolve(__dirname, `../build/contracts/${contract}.json`), 'utf8');
+  contractObject =  JSON.parse(fileData);
+  const mapped = {name: contractObject.contractName, abi: contractObject.abi, address: contractObject.networks[network.toString()].address};  
+  const replacevar = (`<${mapped.name}_address>`);
+  console.log(replacevar)
+  shell.sed('-i', replacevar, `'${mapped.address}'`, path.resolve(__dirname, `../sg_affogato/subgraph.yaml`));
+  fs.writeFile(`${mapped.name}.abi`, JSON.stringify(mapped.abi, null, 2), 'utf8', () => {}) 
+
+  return mapped;  
 });
